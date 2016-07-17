207f910864a9a02b72f1c94c730a8f5f1f049f5e
Pieter Hordijk <info@pieterhordijk.com>
2016-07-17 15:02:40 +0200

    Added base routes

--- routes.php
+++ routes.php
@@ -1,1 +1,21 @@
 <?php declare(strict_types=1);
+
+namespace WebNews;
+
+use Auryn\Injector;
+use CodeCollab\Router\Router;
+use WebNews\Presentation\Controller\Resource;
+use WebNews\Presentation\Controller\Error;
+use WebNews\Presentation\Controller\Index;
+
+/** @var Injector $auryn */
+$router = $auryn->make(Router::class);
+
+$router
+    ->get('/js/{filename:.+}', [Resource::class, 'renderJavascript'])
+    ->get('/css/{filename:.+}', [Resource::class, 'renderStylesheet'])
+    ->get('/fonts/{filename:.+}', [Resource::class, 'renderFont'])
+    ->get('/not-found', [Error::class, 'notFound'])
+    ->get('/method-not-allowed', [Error::class, 'methodNotAllowed'])
+    ->get('/', [Index::class, 'index'])
+;
