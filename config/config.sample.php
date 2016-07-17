954f4849e42cf7394870ad66d4a33166983a2108
Pieter Hordijk <info@pieterhordijk.com>
2016-07-17 13:42:13 +0200

    Added example config

---
+++ config/config.sample.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+$configuration = [
+    // Set to false on production to use cached routes
+    'reloadRoutes'   => true,
+    // The active theme that is used
+    'activeTheme'    => 'Default',
+    // The language used by the application
+    'activeLanguage' => 'en_US',
+];
