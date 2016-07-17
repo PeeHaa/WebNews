bcd229183a041b41d45246ed34e5f1e16e98c3f7
Pieter Hordijk <info@pieterhordijk.com>
2016-07-17 13:41:21 +0200

    Added token storage implementation

---
+++ src/Storage/TokenSession.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace WebNews\Storage;
+
+use CodeCollab\CsrfToken\Storage\Storage;
+use CodeCollab\Http\Session\Session;
+
+class TokenSession implements Storage
+{
+    private $session;
+
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
+    }
+
+    public function exists(string $key): bool
+    {
+        return $this->session->exists($key);
+    }
+
+    public function get(string $key): string
+    {
+        return $this->session->get($key);
+    }
+
+    public function set(string $key, string $token)
+    {
+        $this->session->set($key, $token);
+    }
+}
