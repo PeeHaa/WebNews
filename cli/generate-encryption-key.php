fa7656ad5ae4c9de4ca778a8c537946bff634097
Pieter Hordijk <info@pieterhordijk.com>
2016-07-17 13:43:08 +0200

    Added encrytion key generator CLI script

---
+++ cli/generate-encryption-key.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace WebNews\Cli;
+
+use CodeCollab\Encryption\Defusev2\Key;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+$keyPath = realpath(__DIR__ . '/../data/encryption.key');
+
+if (file_exists(__DIR__ . '/../data/encryption.key')) {
+    echo 'Encryption key already exists in `' . $keyPath . '`.' . PHP_EOL;
+    exit;
+}
+
+try {
+    file_put_contents($keyPath, (new Key())->generate());
+} catch (\Throwable $e) {
+    echo 'Could not generate an encryption key. ' . $e->getMessage() . PHP_EOL;
+    exit;
+}
+
+echo 'A new encryption key has been generated and stored in `' . $keyPath . '`.' . PHP_EOL;
