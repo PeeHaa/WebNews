7eaf26f3553fc5c6da1dda58ca657c4cde892fcb
Pieter Hordijk <info@pieterhordijk.com>
2016-07-21 22:42:03 +0200

    Added full response

---
+++ lib/Nntp/Response/Response.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace PeeHaa\Nntp\Response;
+
+class Response
+{
+    private $statusLine;
+
+    private $data;
+
+    public function __construct(StatusLine $statusLine, array $data)
+    {
+        $this->statusLine = $statusLine;
+        $this->data       = $data;
+    }
+}
