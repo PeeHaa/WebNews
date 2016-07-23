620f5e87397fefffe0e7f5b957d357de06b667bc
Pieter Hordijk <info@pieterhordijk.com>
2016-07-23 18:12:28 +0200

    Added thread retrieval to test script

--- cli/test-connection.php
+++ cli/test-connection.php
@@ -6,6 +6,8 @@ use PeeHaa\Nntp\Connection\Connection;
 use PeeHaa\Nntp\Endpoint\Plain;
 use PeeHaa\Nntp\Client;
 use PeeHaa\Nntp\Command\ListCommand;
+use PeeHaa\Nntp\Command\Group;
+use PeeHaa\Nntp\Command\XOver;
 
 require_once __DIR__ . '/../bootstrap.php';
 
@@ -14,3 +16,8 @@ $connection = new Connection($endpoint);
 $client     = new Client($connection);
 
 $client->sendCommand(new ListCommand());
+$client->sendCommand(new Group('php.announce'));
+
+$threads = $client->sendCommand(new XOver(1, 200));
+
+var_dump($threads->getData());
