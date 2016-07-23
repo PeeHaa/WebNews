9a9bca2c66a90484c17919d4c0458b44d832bd92
Pieter Hordijk <info@pieterhordijk.com>
2016-07-23 20:33:53 +0200

    Implemented retrieval of groups

--- src/Storage/Postgres/Group.php
+++ src/Storage/Postgres/Group.php
@@ -3,6 +3,7 @@
 namespace WebNews\Storage\Postgres;
 
 use PeeHaa\Nntp\Result\ListGroup;
+use WebNews\Domain\GroupCollection;
 
 class Group
 {
@@ -64,5 +65,52 @@ class Group
             'isWritable'    => (int) $group->isWritable(),
             'isModerated'   => (int) $group->isModerated(),
         ]);
+    }
+
+    public function getAll(): GroupCollection
+    {
+        $query = 'SELECT name, low_watermark, high_watermark, is_writable, is_moderated';
+        $query.= ' FROM groups';
+        $query.= ' ORDER BY name ASC';
+
+        $stmt = $this->dbConnection->query($query);
+
+        $groups = array_column($stmt->fetchAll(), null, 'name');
+        $groups = $this->addNumberOfThreads($groups);
+        $groups = $this->addTimestampOfLastMessage($groups);
+
+        return new GroupCollection($groups);
+    }
+
+    private function addNumberOfThreads(array $groups): array
+    {
+        $query = 'SELECT "group", COUNT("group")';
+        $query.= ' FROM threads';
+        $query.= ' GROUP BY "group"';
+
+        $stmt = $this->dbConnection->query($query);
+
+        foreach ($stmt->fetchAll() as $tally) {
+            $groups[$tally['group']]['numberOfThreads'] = $tally['count'];
+        }
+
+        return $groups;
+    }
+
+    private function addTimestampOfLastMessage(array $groups): array
+    {
+        $query = 'SELECT groups.name, MAX(messages.timestamp) as timestamp';
+        $query.= ' FROM messages';
+        $query.= ' JOIN threads ON threads.id = messages.thread';
+        $query.= ' JOIN groups ON groups.name = threads.group';
+        $query.= ' GROUP BY groups.name';
+
+        $stmt = $this->dbConnection->query($query);
+
+        foreach ($stmt->fetchAll() as $tally) {
+            $groups[$tally['name']]['timestamp'] = $tally['timestamp'];
+        }
+
+        return $groups;
     }
 }
